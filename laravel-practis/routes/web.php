8695c98cbaae13d179a9247929484cc2b693fc19
ShibuyaKosuke <github@shibuyakosuke.com>
2023-05-27 16:32:46 +0900

    add test

--- laravel-practis/routes/web.php
+++ laravel-practis/routes/web.php
@@ -32,7 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('companies.sections', \App\Http\Controllers\SectionController::class);
     Route::resource('sections.users', \App\Http\Controllers\SectionUserController::class)->only(['store', 'destroy']);
 
-    Route::get('/api/companies/{company}/sections', \App\Http\Controllers\AjaxSectionController::class);
+    Route::get('/api/companies/{company}/sections', \App\Http\Controllers\AjaxSectionController::class)->name('api.sections.index');
 });
 
 require __DIR__.'/auth.php';
